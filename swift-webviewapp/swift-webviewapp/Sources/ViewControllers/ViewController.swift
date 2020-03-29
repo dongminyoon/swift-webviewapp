47424328c44f2a1fad0cce5cc2ac4a34825a24d8
dongminyoon <ehdals8965@naver.com>
2020-03-29 09:55:30 +0900

    Reachablilty 판단 로직 추가

--- swift-webviewapp/swift-webviewapp/Sources/ViewControllers/ViewController.swift
+++ swift-webviewapp/swift-webviewapp/Sources/ViewControllers/ViewController.swift
@@ -8,23 +8,48 @@
 
 import UIKit
 import WebKit
+import Alamofire
 
 class ViewController: UIViewController {
     @IBOutlet weak var webView: WKWebView!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        webView.uiDelegate = self
-        webView.navigationDelegate = self
-        
-        guard let url = URL(string: "https://m.shopping.naver.com") else { return }
+        loadWebPage()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        guard let isReachable = NetworkReachabilityManager()?.isReachable else { return }
+        if isReachable {
+            print("Connect")
+        } else {
+            let alertController = UIAlertController(title: "네트워크 오류", message: "네트워크 연결이 필요합니다.", preferredStyle: .alert)
+            let alertAction = UIAlertAction(title: "확인", style: .cancel) { _ in
+                exit(0)
+            }
+            alertController.addAction(alertAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+    }
+
+    private func loadWebPage() {
+        guard let url = URL(string: "https://www.coupang.com/") else { return }
         let request = URLRequest(url: url)
         webView.load(request)
     }
 }
 
 extension ViewController: WKUIDelegate {
+    func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration, for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
+        let configuration = WKWebViewConfiguration()
+        let contentController = WKUserContentController()
+        let userScript = WKUserScript(source: "", injectionTime: .atDocumentEnd, forMainFrameOnly: false)
+        contentController.addUserScript(userScript)
+        configuration.userContentController = contentController
+        return WKWebView(frame: webView.frame, configuration: configuration)
+    }
+    
     func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping () -> Void) {
         let alertController = UIAlertController(title: message, message: nil, preferredStyle: .alert)
         let cancelAction = UIAlertAction(title: "확인", style: .cancel) { _ in
@@ -55,6 +80,8 @@ extension ViewController: WKUIDelegate {
 }
 
 extension ViewController: WKNavigationDelegate {
-    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        
+    }
 }
 
